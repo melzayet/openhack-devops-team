94771f04159d36b3c18d553e02f9a7549cc15929
melzayet <maelzaye@microsoft.com>
2021-01-25 16:38:44 +0000

    two

--- apis/poi/web/Controllers/HealthcheckController.cs
+++ apis/poi/web/Controllers/HealthcheckController.cs
@@ -27,7 +27,7 @@ namespace poi.Controllers
         [Produces("application/json", Type = typeof(Healthcheck))]
         public IActionResult Get()
         {
-            _logger.LogInformation(LoggingEvents.Healthcheck, "Healthcheck one Requested");
+            _logger.LogInformation(LoggingEvents.Healthcheck, "Healthcheck two Requested");
             return Ok(new Healthcheck());
         }
     }
